deac6fb044ff709a35a8207ad8a4fb1c4ab063b4
cwille2012 <CHRIS@CHRISTOPHERWILLE.COM>
2018-04-13 02:05:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/', express.static(path.join(__dirname)));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 9600;
+const port = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV;
 
 var dbHost = "localhost";
