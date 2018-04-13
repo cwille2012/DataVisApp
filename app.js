8a672e34657071d56e9c7e583fb25385ed888e2a
cwille2012 <CHRIS@CHRISTOPHERWILLE.COM>
2018-04-13 01:50:25 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,173 @@
+/* global window,document */
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import MapGL from 'react-map-gl';
+import DeckGLOverlay from './deckgl-overlay.js';
+
+import { csv as requestCsv } from 'd3-request';
+
+const serverURL = 'http://localhost';
+const serverPORT = 9600;
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiY3dpbGxlMjAxMiIsImEiOiJjajJxdWJyeXEwMDE5MzNydXF2cm1sbDU1In0.kCKIz6Ivh3EfNOmEfTANOA';
+
+class Root extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                ...DeckGLOverlay.defaultViewport,
+                width: 500,
+                height: 500
+            },
+            data: null
+        };
+        var _this = this;
+
+        function httpGetAsync(theUrl, callback) {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                    callback(xmlHttp.responseText);
+            }
+            xmlHttp.open("GET", theUrl, true);
+            xmlHttp.send(null);
+        }
+
+        this._onHover = this._onHover.bind(this);
+
+        var canPoll;
+
+        function pollFunc(fn, timeout, interval) {
+            var startTime = (new Date()).getTime();
+            interval = interval || 1000,
+                canPoll = true;
+
+            (function p() {
+                canPoll = ((new Date).getTime() - startTime) <= timeout;
+                if (!fn() && canPoll) { // ensures the function exucutes
+                    setTimeout(p, interval);
+                }
+            })();
+        }
+
+        pollFunc(getData, 45000, 7000);
+
+        function getData() {
+            //console.log('Updating data');
+            var address = serverURL + ':' + serverPORT;
+            httpGetAsync(address + '/locationdata', function(response) {
+                var newData = JSON.parse(response);
+                const newResponse = newData.map(d => [Number(d.long), Number(d.lat)]);
+                //console.log("New dataset:");
+                console.log(newResponse);
+                const data = newResponse;
+
+                _this.setState({ data });
+            });
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._resize.bind(this));
+        this._resize();
+    }
+
+    _resize() {
+        this._onViewportChange({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    }
+
+    _onViewportChange(viewport) {
+        this.setState({
+            viewport: {...this.state.viewport, ...viewport }
+        });
+    }
+
+    _onHover({ x, y, object }) {
+        this.setState({ x, y, hoveredObject: object });
+    }
+
+    _onMouseMove(evt) {
+        if (evt.nativeEvent) {
+            this.setState({ mousePosition: [evt.nativeEvent.offsetX, evt.nativeEvent.offsetY] });
+        }
+    }
+
+    _onMouseEnter() {
+        this.setState({ mouseEntered: true });
+    }
+
+    _onMouseLeave() {
+        this.setState({ mouseEntered: false });
+    }
+
+    _renderTooltip() {
+        const { x, y, hoveredObject } = this.state;
+        var myx = x + 15;
+        var myy = y + 15;
+
+        if (!hoveredObject) {
+            return null;
+        }
+
+        var tooltipExists = !!document.getElementById('tooltip');
+
+        if (tooltipExists) {
+            document.getElementById('tooltip').style.position = "absolute";
+            document.getElementById('tooltip').style.zIndex = 99999;
+            document.getElementById('tooltip').style.color = '#fff';
+            document.getElementById('tooltip').style.background = 'rgba(0, 0, 0, 0.8)';
+            document.getElementById('tooltip').style.padding = '4px';
+            document.getElementById('tooltip').style.fontSize = '18px';
+            document.getElementById('tooltip').style.maxWidth = '300px';
+            document.getElementById('tooltip').style.left = myx + 'px';
+            document.getElementById('tooltip').style.top = myy + 'px';
+            document.getElementById('tooltip').style.cursor = 'pointer';
+            document.getElementById('tooltip').setAttribute('text-decoration', 'none!important');
+            //console.log(hoveredObject);
+            //codeLatLng(hoveredObject.centroid[1], hoveredObject.centroid[0]);
+        }
+
+        return ( < div id = "tooltip"
+            style = {
+                { left: myx, top: myy }
+            } >
+            <
+            div > { 'Speed: ' + hoveredObject.points.length + ' mph' } < /div> <
+            div > { 'Longitude: ' + hoveredObject.centroid[0] } < /div> <
+            div > { 'Latitude: ' + hoveredObject.centroid[1] } < /div> <
+            div > { 'Index: ' + hoveredObject.index } < /div>  < /
+            div >
+        );
+    }
+
+
+    render() {
+        const { viewport, data, iconMapping, mousePosition, mouseEntered } = this.state;
+
+        return ( <
+            div onMouseMove = { this._onMouseMove.bind(this) }
+            onMouseEnter = { this._onMouseEnter.bind(this) }
+            onMouseLeave = { this._onMouseLeave.bind(this) } > { this._renderTooltip() } <
+            MapGL {...viewport }
+            mapStyle = "mapbox://styles/mapbox/dark-v9"
+            onViewportChange = { this._onViewportChange.bind(this) }
+            mapboxApiAccessToken = { MAPBOX_TOKEN } >
+            <
+            DeckGLOverlay viewport = { viewport }
+            data = { data || [] }
+            mousePosition = { mousePosition }
+            mouseEntered = { mouseEntered }
+            onHover = { this._onHover.bind(this) }
+            /> < /
+            MapGL >
+            <
+            /div>
+        );
+    }
+}
+
+render( < Root / > , document.body.appendChild(document.createElement('div')));
---
+++ server.js
@@ -0,0 +1,179 @@
+const compress = require('compression');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const request = require('request');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const sys = require('util');
+const exec = require('child_process').exec;
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors())
+app.disable('etag');
+app.use(compress());
+app.use('/', express.static(path.join(__dirname)));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 9600;
+const environment = process.env.NODE_ENV;
+
+var dbHost = "localhost";
+var dbPort = 27017;
+var databaseName = "locationmotivation";
+var databaseURL = 'mongodb://' + dbHost + ':' + dbPort + '/' + databaseName;
+
+console.log("Starting server");
+console.log("Server started");
+
+console.log("Waiting for data...");
+console.log('------------------------------------------------');
+
+app.get('/delete/locationdata', (req, res) => {
+    MongoClient.connect(databaseURL, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("datavisproject");
+        var myquery = {};
+        db.collection("locationdata").deleteMany(myquery, function(err, obj) {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(obj.result.n + " document(s) deleted");
+            db.close();
+        });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html', (err, html) => {
+        if (err) {
+            res.end("Not found");
+        }
+    });
+});
+
+app.get('/locationdata', (req, res) => {
+    MongoClient.connect(databaseURL, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("datavisproject");
+        dbo.collection("location").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            var locationObject = [];
+            for (var i in result) {
+                var date = result[i]['date'];
+                var time = result[i]['time'];
+                var lat = result[i]['location'][0];
+                var long = result[i]['location'][1];
+                var item = { date: date, time: time, lat: lat, long: long };
+                locationObject.push(item);
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(locationObject));
+            db.close();
+        });
+    });
+});
+
+app.get('/locations', (req, res) => {
+    MongoClient.connect(databaseURL, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("datavisproject");
+        dbo.collection("locations").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            var locationObject = [];
+            for (var i in result) {
+                var locationName = result[i]['name'];
+                var lat = result[i]['location'][0];
+                var long = result[i]['location'][1];
+                var radius = result[i]['radius'];
+                var item = { name: locationName, radius: radius, lat: lat, long: long };
+                locationObject.push(item);
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(locationObject));
+            db.close();
+        });
+    });
+});
+
+app.post('/locationdata', function(req, res) {
+    console.log("Received data:");
+    console.log(req.body);
+
+    MongoClient.connect(databaseURL, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("datavisproject");
+        var ctime = (new Date).getTime();
+        var stime = String(new Date().timeNow());
+        var sdate = String(new Date().today());
+        var newObj = {
+            epoch: ctime,
+            date: sdate,
+            time: stime,
+            location: [Number(req.body.lat), Number(req.body.long)]
+        };
+        console.log(newObj);
+        dbo.collection("location").insertOne(newObj, function(err, res) {
+            if (err) throw err;
+            console.log("Data saved");
+            console.log('------------------------------------------------');
+            db.close();
+        });
+    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('ok');
+});
+
+app.post('/newlocation', function(req, res) {
+    console.log("Received data:");
+    console.log(req.body);
+
+    MongoClient.connect(databaseURL, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("datavisproject");
+        var newObj = {
+            name: req.body.name,
+            radius: req.body.radius,
+            location: [Number(req.body.lat), Number(req.body.long)]
+        };
+        console.log(newObj);
+        dbo.collection("locations").insertOne(newObj, function(err, res) {
+            if (err) throw err;
+            console.log("Data saved");
+            console.log('------------------------------------------------');
+            db.close();
+        });
+    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('ok');
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/404.jpg', (err, html) => {
+        if (err) {
+            res.end("Not found");
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+// For todays date;
+Date.prototype.today = function() {
+    return (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + ((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + this.getFullYear();
+}
+
+// For the time now
+Date.prototype.timeNow = function() {
+    var currentHour = Number(((this.getHours() < 10) ? "0" : "") + this.getHours());
+    var ampm = 'AM';
+    if (currentHour > 12) {
+        currentHour = currentHour - 12;
+        ampm = 'PM';
+    }
+    return currentHour + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds() + ' ' + ampm;
+}
